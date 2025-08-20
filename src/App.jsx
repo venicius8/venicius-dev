e3b70235b9999dc66ca4b7fa300aa9f78926debf
Venícius <miguelsilva8v@gmail.com>
2025-08-20 12:15:31 -0300

    style: semantic page and beautiful main

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,72 @@
 import { Header } from "./components/Header.jsx";
+import Styles from "./App.module.css";
 
 function App() {
   return (
     <>
       <Header></Header>
-      <h1>
-        Hey! It's me, <span className="nameEffect">Venícius Miguel</span>
-      </h1>
 
-      <h2 id="history">Minha história</h2>
-      <p>
-        Sou do interior pernambucano, tenho 18 anos, e minha maior paixão é a{" "}
-        <u>tecnologia</u>. Todo esse amor veio de...
-      </p>
+      <section className={Styles.main}>
+        <h1>
+          Hey! It's me,{" "}
+          <span className={Styles.nameEffect}>Venícius Miguel</span>
+        </h1>
+      </section>
 
-      <h2>E sobre hoje</h2>
-      <p>
-        Atualmente, estou comprometido a estudar desenvolvimento web 5 horas por
-        dia, 5 dias por semanas, desde início de 2025, sem nenhuma falta*. Com
-        esse período de tempo, consegui adquirir diversos conhecimentos, como:
-      </p>
-      <div className="knowledgeChart"></div>
-      <p>Além de possuir experiência em:</p>
-      <div className="experienceChart"></div>
+      <section>
+        <h2 id="history">Minha história</h2>
+        <p>
+          Sou do interior pernambucano, tenho 18 anos, e minha maior paixão é a{" "}
+          <u>tecnologia</u>. Tudo comecou quando eu... "Ler mais"
+        </p>
+      </section>
 
-      <h2 id="whatIDid">O que já fiz</h2>
-      <p>
-        Tenho um portfólio onde mostro todos os meus projetos principais que
-        construi.
-      </p>
-      <div className="portfolio"></div>
+      <section>
+        <h2>E sobre hoje</h2>
+        <p>
+          Atualmente, estou comprometido a estudar desenvolvimento web 5 horas
+          por dia, 5 dias por semanas, desde início de 2025, sem nenhuma falta*.
+          Com esse período de tempo, consegui adquirir diversos conhecimentos,
+          como:
+        </p>
+        <div className="knowledgeChart"></div>
+        <p>Além de possuir experiência em:</p>
+        <div className="experienceChart"></div>
+      </section>
 
-      <h2 id="platafoms">Minhas as plataformas</h2>
-      <div className="twitterLink"></div>
-      <p>
-        Aqui eu faço um desafio do #100DaysOfCode, onde deve fazer 100 dias
-        consecutivos escrevendo códigos e documentando o processo.
-      </p>
-      <div className="githubRepoLink"></div>
-      <p>
-        Não precisa agora perdido em meus repositórios. Neste link, mostro os
-        repositórios mais úteis e segmentados. Obs.: é diferente do portfólio!
-      </p>
+      <section>
+        <h2 id="whatIDid">O que já fiz</h2>
+        <p>
+          Tenho um portfólio onde mostro todos os meus projetos principais que
+          construi.
+        </p>
+        <div className="portfolio"></div>
+      </section>
 
-      <h2>Contato</h2>
-      <a href="mailto:miguelsilva8v@gmail.com">miguelsilva8v@gmail.com</a>
-      <a href="http://x.com/venicius8v">x.com/venicius8v</a>
+      <section>
+        <h2 id="platafoms">Minhas as plataformas</h2>
+        <div className="twitterLink"></div>
+        <p>
+          Aqui eu faço um desafio do #100DaysOfCode, onde deve fazer 100 dias
+          consecutivos escrevendo códigos e documentando o processo.
+        </p>
+        <div className="githubRepoLink"></div>
+        <p>
+          Não precisa agora perdido em meus repositórios. Neste link, mostro os
+          repositórios mais úteis e segmentados. Obs.: é diferente do portfólio!
+        </p>
+      </section>
 
-      <h2>Observações</h2>
-      <p>
-        * Fiquei um período sem computador, logo perdi 2 semanas de estudos.
-      </p>
+      <footer>
+        <h2>Contato</h2>
+        <a href="mailto:miguelsilva8v@gmail.com">miguelsilva8v@gmail.com</a>
+        <a href="http://x.com/venicius8v">x.com/venicius8v</a>
+
+        <h2>Observações</h2>
+        <p>
+          * Fiquei um período sem computador, logo perdi 2 semanas de estudos.
+        </p>
+      </footer>
     </>
   );
 }
